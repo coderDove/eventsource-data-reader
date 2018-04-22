2360966c673069eb0df1b2a0686bd83848739a4c
Anton Holub <coder.dove@gmail.com>
2018-04-23 02:40:37 +0300

    - Added sorting of new series measurements by timestamp - Added checking if new measurement is newer than already existed

--- NetronixDataReader/Helpers/NDREventSourceReader.swift
+++ NetronixDataReader/Helpers/NDREventSourceReader.swift
@@ -41,8 +41,9 @@ final class NDREventSourceReader {
     private func eventSourceEventRecieved(event: Event?) {
         if let jsonData = event?.data.data(using: .utf8) {
             if let newSeries = try? JSONDecoder().decode([NDREventSerie].self, from: jsonData) {
-                self.processAdding(of: newSeries)
-                self.delegate?.eventSourceReaderDidReceiveNewDataPackage()
+                if (self.add(series: newSeries)) {
+                    self.delegate?.eventSourceReaderDidReceiveNewDataPackage()
+                }
             }
         }
     }
@@ -52,15 +53,30 @@ final class NDREventSourceReader {
     }
     
     // MARK: - Private instance
-    private func processAdding(of series: [NDREventSerie]) {
+    private func add(series: [NDREventSerie]) -> Bool {
+        var seriesDataUpdated = false
         for eventSerie in series {
             if let existedSerieIndex = self.latestData.index(where: { $0.name == eventSerie.name }) {
+                // Updating serie if same serie name was alread existed
                 if (eventSerie.measurements.count > 0) {
-                    self.latestData[existedSerieIndex] = eventSerie
+                    // Making measurement with latest timestamp be on first place
+                    eventSerie.measurements.sort { $0.timestamp > $1.timestamp }
+                    
+                    // Updating serie with existed name only if new timestamp is newer than existed
+                    let existedSerieTimestamp = self.latestData[existedSerieIndex].measurements.first?.timestamp ?? 0
+                    let newSerieTimestamp = eventSerie.measurements.first?.timestamp ?? 0
+                    if (existedSerieTimestamp < newSerieTimestamp) {
+                        self.latestData[existedSerieIndex] = eventSerie
+                        seriesDataUpdated = true
+                    }
                 }
             } else {
+                // Adding serie with non-existed name
                 self.latestData.append(eventSerie)
+                seriesDataUpdated = true
             }
         }
+        
+        return seriesDataUpdated
     }
 }
