c857b4c2c712d85bdd011036ee1514b0d2306a4f
Anton Holub <coder.dove@gmail.com>
2018-04-22 22:22:22 +0300

    - Added protocol for event source data reader - Implemented adding of newly received series and storing only the latest one values

--- NetronixDataReader/Helpers/NDREventSourceReader.swift
+++ NetronixDataReader/Helpers/NDREventSourceReader.swift
@@ -8,10 +8,20 @@
 
 import EventSource
 
-final class NDREventSourceReader: NSObject {
+protocol NDREventSourceReadarListener: class {
+    func eventSourceReaderDidEstablishConnection()
+    func eventSourceReaderDidReceiveNewDataPackage()
+    func eventSourceReaderDidReceiveError()
+}
+
+final class NDREventSourceReader {
     static let sharedInstacne = NDREventSourceReader()
     
-    var eventSource: EventSource?
+    private var eventSource: EventSource?
+    
+    weak var delegate: NDREventSourceReadarListener?
+    var latestData: [NDREventSerie]? = [NDREventSerie]()
+    
     
     func startListening(from url: URL) {
         if (self.eventSource == nil) {
@@ -24,14 +34,30 @@ final class NDREventSourceReader: NSObject {
     
     // MARK: - EventSource callbacks
     private func eventSourceConnectionEstablished(event: Event?) {
-        
+        self.delegate?.eventSourceReaderDidEstablishConnection()
     }
     
     private func eventSourceEventRecieved(event: Event?) {
-        
+        if let jsonData = event?.data.data(using: .utf8) {
+            if let newSeries = try? JSONDecoder().decode([NDREventSerie].self, from: jsonData) {
+                self.processAdding(of: newSeries)
+                self.delegate?.eventSourceReaderDidReceiveNewDataPackage()
+            }
+        }
     }
     
     private func eventSourceError(event: Event?) {
-        
+        self.delegate?.eventSourceReaderDidReceiveError()
+    }
+    
+    // MARK: - Private instance
+    private func processAdding(of series: [NDREventSerie]) {
+        for eventSerie in series {
+            if let existedSerie = self.latestData?.first(where: { $0.name == eventSerie.name }) {
+                existedSerie.measurments = eventSerie.measurments
+            } else {
+                self.latestData?.append(eventSerie)
+            }
+        }
     }
 }
